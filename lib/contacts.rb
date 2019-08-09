8a4fc843f40cf7d3b5d8e8f966e7fbb21825cbdf
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-08-09 03:08:40 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,7 +15,7 @@ require 'pry'
   # }
 
 def remove_strawberry(contacts)
-  if person=="Freddy Mercury"
+  if person== "Freddy Mercury"
   contacts.each do |person, contact_details_hash|
       coontact_details_hash.each do |attribute,data|
     binding.pry
